c5cd38a941517de09b5b95f2ee8a072a9872482f
PhotoGrammer Kurt <114688193+RoTSoL03@users.noreply.github.com>
2024-11-19 14:31:50 +0800

    Update speedtest.py

--- speedtest.py
+++ speedtest.py
@@ -4,3 +4,18 @@ from tkinter import *
 from tkinter import messagebox, filedaialog, ttk
 
 root = tk.Tk()
+root.title("Python Speed Tester")
+
+def speed_test():
+  try:
+    st = speedtest.Speedtest()
+    st.get_best_server()
+    #Perform download and upload test
+    download_speed = st.download() / 1_000_000 #comvert from bits to megabits
+    upload_speed = st.upload() / 1_000_000 #Convert from bits to megabits
+    down_label.config(text=f"Download Speed: {download_speed:.2f} Mbps")
+    up_label.config(text=f"Upload Speed: {upload_speed:.2f} Mbps")
+  except Exception as e:
+    messagebox.showerror("Error", f"An error occured {str(e)}")
+
+title_label = tk.Label(root, text="Speed Test", font("Times", 30), bg="aliceblue")
