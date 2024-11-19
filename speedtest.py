d83f8c4fc3ae15dddc9146f4a83143dd92a38953
PhotoGrammer Kurt <114688193+RoTSoL03@users.noreply.github.com>
2024-11-19 14:32:46 +0800

    Update speedtest.py

--- speedtest.py
+++ speedtest.py
@@ -19,3 +19,14 @@ def speed_test():
     messagebox.showerror("Error", f"An error occured {str(e)}")
 
 title_label = tk.Label(root, text="Speed Test", font("Times", 30), bg="aliceblue")
+title_label.place(x=170, y=20)
+
+down_label = tk.Label(root, text="Download Speed: ", font=("Helvetica", 15), bg="aliceblue")
+down_label.place(x=100, y=120)
+up_label = tk.Label(root, text="Upload Speed: ", font=("Helvetica", 15), bg="aliceblue")
+up_label.place(x=100, y=150)
+
+test_btn = tk.Button(root, width=10, height=1, text='Test', border=1, bg='blue', cursor='hand2', fg='white', font=("Helvetica", 20), command=speed_test)
+test_btn.place(x=180, y=270)
+
+root.mainloop()
